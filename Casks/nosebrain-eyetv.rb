5a6cdb761ca64a0704684772d60ac7d6721f5242
Daniel Zoller <nosebrain@gmx.net>
2015-10-30 23:12:26 +0100

    fixed build number

--- Casks/nosebrain-eyetv.rb
+++ Casks/nosebrain-eyetv.rb
@@ -2,7 +2,7 @@ cask :v1 => 'nosebrain-eyetv' do
   version '3.6.8'
   sha256 '7dea4cdd5f9feb643a45f60ccb93dcf5983dd13946b95621a86dd5b06b87e1d7'
   
-  url "http://files.elgato.com/eyetvdownloads/support/eyetv_#{version.delete('.')}_7405.dmg"
+  url "http://files.elgato.com/eyetvdownloads/support/eyetv_#{version.delete('.')}_7407.dmg"
   name 'EyeTV'
   homepage 'https://www.elgato.com/'
   license :gratis
